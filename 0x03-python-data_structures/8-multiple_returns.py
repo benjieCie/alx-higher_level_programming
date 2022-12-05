45e8807c8a1e0c0ddbfeeee64b7595b6801c7c27
benjieCie <musili.benjamin@gmail.com>
2022-12-05 10:05:31 -0800

    Chaned logic

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
+    if sentence = '':
+        first = None
+    else:
+        first = sentence[0]
 
-    return length, first
+    return (len(sentence), first)
