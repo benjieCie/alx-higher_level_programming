9c558f68ee3bfd85f53d3be4e69f283cb5e96dfa
benjieCie <musili.benjamin@gmail.com>
2022-12-05 10:08:06 -0800

    Changed logic again

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence = '':
-        first = None
-    else:
+    if sentence != '':
         first = sentence[0]
+    else:
+        first = None
 
     return (len(sentence), first)
