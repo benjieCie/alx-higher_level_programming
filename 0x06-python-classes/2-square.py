a227eba8820122edd9dae412c691c99085467a00
benjieCie <musili.benjamin@gmail.com>
2022-12-20 09:47:11 -0800

    Corrected logic

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """A square class with private attribute size"""
     def __init__(self, size=0):
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >=0")
