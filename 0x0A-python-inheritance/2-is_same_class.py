1cf802608c506c83bb81c7c06c6f2dbd4225e6bd
benjieCie <musili.benjamin@gmail.com>
2023-01-09 09:47:17 -0800

    Added Documentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Checks if onject is exact instace of a class"""
+
+
 def is_same_class(obj, a_class):
     """function that returns True if the object is exactly
     an instance of the specified clas otherwise False"""
