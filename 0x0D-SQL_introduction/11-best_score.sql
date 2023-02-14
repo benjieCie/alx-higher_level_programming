af9f738594e8469c20a2606903b8bb72f18a13da
benjieCie <musili.benjamin@gmail.com>
2023-02-14 08:43:41 -0800

    Select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- lists all records of table second_table with score >=10
+-- should list both score and name
+-- order by score
+SELECT score, name FROM second_table WHERE score>=10 ORDER BY score DESC;
